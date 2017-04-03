af5328d691572c4257bcc5826fb2d73a79e4aef4
tobiias1 <toh22993@dsiay.com>
2017-04-03 17:26:42 +0200

    An-&Aus verwchselt

--- sockets/base.js
+++ sockets/base.js
@@ -27,10 +27,10 @@ module.exports = function (io) {
              }
             else if(zustand == -1)
              {
-                 request("http://127.0.0.1:8000/?relay=1&status=0", function(error, response, body) {
+                 request("http://127.0.0.1:8000/?relay=0&status=0", function(error, response, body) {
                      console.log('error:', error);
                      console.log(body);
-                    request("http://127.0.0.1:8000/?relay=0&status=1", function(error, response, body) {
+                    request("http://127.0.0.1:8000/?relay=1&status=1", function(error, response, body) {
                         console.log('error:', error);
                         console.log(body);
                         });
