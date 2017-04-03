e39f2dcfbb99548f3d6686755de903dfac8dca1d
tobiias1 <toh22993@dsiay.com>
2017-04-03 15:10:05 +0200

    Bugfix: zweites Relai ausschalten + auschalten bei Richtungswechsel

--- sockets/base.js
+++ sockets/base.js
@@ -8,19 +8,32 @@ module.exports = function (io) {
                      console.log('error:', error);
                      console.log(body);
                     });
-             }
-             else if(zustand == 1)
-             {
-                 request("http://127.0.0.1:8000/?relay=0&status=1", function(error, response, body) {
+
+                 request("http://127.0.0.1:8000/?relay=1&status=0", function(error, response, body) {
                      console.log('error:', error);
                      console.log(body);
                     });
              }
-            else if(zustand == -1)
+             else if(zustand == 1)
              {
-                 request("http://127.0.0.1:8000/?relay=1&status=1", function(error, response, body) {
+               request("http://127.0.0.1:8000/?relay=1&status=0", function(error, response, body) {
                      console.log('error:', error);
                      console.log(body);
+                    request("http://127.0.0.1:8000/?relay=0&status=1", function(error, response, body) {
+                        console.log('error:', error);
+                        console.log(body);
+                        });
+                    });
+             }
+            else if(zustand == -1)
+             {
+                 request("http://127.0.0.1:8000/?relay=1&status=0", function(error, response, body) {
+                     console.log('error:', error);
+                     console.log(body);
+                    request("http://127.0.0.1:8000/?relay=0&status=1", function(error, response, body) {
+                        console.log('error:', error);
+                        console.log(body);
+                        });
                     });
              }
              console.log(zustand);
