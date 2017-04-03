796c46351d05fb0330766ef868530138e2136d19
tobiias1 <toh22993@dsiay.com>
2017-04-03 15:00:59 +0200

    Ansteuerung der REST API

--- sockets/base.js
+++ sockets/base.js
@@ -1,6 +1,28 @@
 module.exports = function (io) {
     io.on('connection', function(socket){
          socket.on('motor1', function(zustand){
+             var request = require("request");
+             if(zustand == 0)
+             {
+                 request("http://127.0.0.1:8000/?relay=0&status=0", function(error, response, body) {
+                     console.log('error:', error);
+                     console.log(body);
+                    });
+             }
+             else if(zustand == 1)
+             {
+                 request("http://127.0.0.1:8000/?relay=0&status=1", function(error, response, body) {
+                     console.log('error:', error);
+                     console.log(body);
+                    });
+             }
+            else if(zustand == -1)
+             {
+                 request("http://127.0.0.1:8000/?relay=1&status=1", function(error, response, body) {
+                     console.log('error:', error);
+                     console.log(body);
+                    });
+             }
              console.log(zustand);
          });
     });
