68ae5f96eacb731a2c289a78ba2d9ac8a859026f
Tobias <tobias@dreyem.de>
2017-05-23 12:21:10 +0200

    Anpassungen für zwei Motoren

--- sockets/base.js
+++ sockets/base.js
@@ -4,36 +4,50 @@ module.exports = function (io) {
              var request = require("request");
              if(zustand == 0)
              {
-                 request("http://127.0.0.1:8000/?relay=0&status=0", function(error, response, body) {
-                     if(error)console.log('error:', error);
-                     console.log(body);
-                    });
-
-                 request("http://127.0.0.1:8000/?relay=1&status=0", function(error, response, body) {
+                 request("http://127.0.0.1:8000/?motor=1&status=0", function(error, response, body) {
                      if(error)console.log('error:', error);
                      console.log(body);
                     });
              }
              else if(zustand == 1)
              {
-               request("http://127.0.0.1:8000/?relay=1&status=0", function(error, response, body) {
+               request("http://127.0.0.1:8000/?motor=1&status=1", function(error, response, body) {
                      if(error)console.log('error:', error);
                      console.log(body);
-                    request("http://127.0.0.1:8000/?relay=0&status=1", function(error, response, body) {
-                        if(error)console.log('error:', error);
-                        console.log(body);
-                        });
                     });
              }
             else if(zustand == -1)
              {
-                 request("http://127.0.0.1:8000/?relay=0&status=0", function(error, response, body) {
+                 request("http://127.0.0.1:8000/?motor=1&status=-1", function(error, response, body) {
                      if(error)console.log('error:', error);
                      console.log(body);
-                    request("http://127.0.0.1:8000/?relay=1&status=1", function(error, response, body) {
-                        if(error)console.log('error:', error);
-                        console.log(body);
-                        });
+                    });
+             }
+             console.log(zustand);
+         });
+
+
+        socket.on('motor2', function(zustand){
+             var request = require("request");
+             if(zustand == 0)
+             {
+                 request("http://127.0.0.1:8000/?motor=2&status=0", function(error, response, body) {
+                     if(error)console.log('error:', error);
+                     console.log(body);
+                    });
+             }
+             else if(zustand == 1)
+             {
+               request("http://127.0.0.1:8000/?motor=2&status=1", function(error, response, body) {
+                     if(error)console.log('error:', error);
+                     console.log(body);
+                    });
+             }
+            else if(zustand == -1)
+             {
+                 request("http://127.0.0.1:8000/?motor=2&status=-1", function(error, response, body) {
+                     if(error)console.log('error:', error);
+                     console.log(body);
                     });
              }
              console.log(zustand);
